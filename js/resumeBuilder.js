0bc770fd538e8debd870474bf28adc064c2240fa
andreiRicardo <andreiricardorodrigues@gmail.com>
2017-07-22 15:43:54 -0300

    exercicio replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,7 +10,12 @@ $("#main").append(awesomeThougs);
 console.log(awesomeThougs); */
 
 /*  usando replace  */
-var email = "andreiricardorodrigues@email.com";
+/* var email = "andreiricardorodrigues@email.com";
 var newemail = email.replace("email","gmail");
 console.log("email errado\n ",email,"\n email correto \n", newemail);
-$("#main").append("email errado\n <br>",email,"<br>\n email correto \n<br>", newemail);
+$("#main").append("email errado\n <br>",email,"<br>\n email correto \n<br>", newemail); */
+
+var sTring = "Sou o Andrei e me sinto fantastico!";
+var newsTring = sTring.replace("fantastico","engracado");
+console.log("primeiro texto\n",sTring,"\n Segundo texto\n",newsTring);
+$("#main").append("primeiro texto\n<br>",sTring,"<br>\n Segundo texto\n<br>",newsTring);
