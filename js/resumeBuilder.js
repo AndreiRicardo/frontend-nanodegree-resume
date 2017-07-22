4e594339d7f4c4cca234a54f5eb7c910a0efc44b
andreiRicardo <andreiricardorodrigues@gmail.com>
2017-07-22 15:07:24 -0300

    usando variaveis

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,6 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append("Andrei Ricardo Rodrigues");
+var firstname = "andrei";
+var age = 35;
+console.log(firstname);
