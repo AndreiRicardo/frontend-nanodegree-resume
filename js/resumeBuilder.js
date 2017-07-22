9f77c48a79ed3acdb508d84ce5e0e391e026544b
andreiRicardo <andreiricardorodrigues@gmail.com>
2017-07-22 15:33:52 -0300

    replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,6 +5,12 @@ This is empty on purpose! Your code to build the resume will go here.
 var firstname = "andrei";
 var age = 35;
 console.log(firstname); */
-var awesomeThougs = "I am andrei and I am AWESOME !";
+/* var awesomeThougs = "I am andrei and I am AWESOME !";
 $("#main").append(awesomeThougs);
-console.log(awesomeThougs);
+console.log(awesomeThougs); */
+
+/* usando replace
+var email = "andreiricardorodrigues@email.com";
+var newemail = email.replace("email","gmail");
+console.log("email errado\n ",email,"\n email correto \n", newemail);
+ */
