7d9c3ccf8600a32fdba3cb31a2c06c4648f88c24
andreiRicardo <andreiricardorodrigues@gmail.com>
2017-07-22 15:21:30 -0300

    first var test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-$("#main").append("Andrei Ricardo Rodrigues");
+/* $("#main").append("Andrei Ricardo Rodrigues");
 var firstname = "andrei";
 var age = 35;
-console.log(firstname);
+console.log(firstname); */
+var awesomeThougs = "I am andrei and I am AWESOME !";
+$("#main").append(awesomeThougs);
+console.log(awesomeThougs);
