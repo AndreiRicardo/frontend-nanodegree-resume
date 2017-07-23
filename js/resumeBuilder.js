446f9f7f805e3cb6b8d3c90af0ccb29ae68d47df
andreiRicardo <andreiricardorodrigues@gmail.com>
2017-07-23 11:35:04 -0300

    brincndo com array

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -51,12 +51,12 @@ $("#main").append(skills.length); */
 
 var fruits = ['Apple', 'Banana'];
 
-console.log(fruits.length);
+//console.log(fruits.length);
 // 2
 
 //Access (index into) an Array item
 
-var first = fruits[0];
+ //var first = fruits[0];
 // Apple
 
 var last = fruits[fruits.length - 1];
@@ -72,57 +72,57 @@ fruits.forEach(function(item, index, array) {
 
 //Add to the end of an Array
 
-var newLength = fruits.push('Orange');
+//var newLength = fruits.push('Orange');
 // ["Apple", "Banana", "Orange"]
 
 //Remove from the end of an Array
 
-var last = fruits.pop(); // remove Orange (from the end)
+//var last = fruits.pop(); // remove Orange (from the end)
 // ["Apple", "Banana"];
 
 //Remove from the front of an Array
 
-var first = fruits.shift(); // remove Apple from the front
+//var first = fruits.shift(); // remove Apple from the front
 // ["Banana"];
 
 //Add to the front of an Array
 
-var newLength = fruits.unshift('Strawberry') // add to the front
+//var newLength = fruits.unshift('Strawberry') // add to the front
 // ["Strawberry", "Banana"];
 
 //Find the index of an item in the Array
 
-fruits.push('Mango');
+//fruits.push('Mango');
 // ["Strawberry", "Banana", "Mango"]
 
-var pos = fruits.indexOf('Banana');
+//var pos = fruits.indexOf('Banana');
 // 1
 
 //Remove an item by index position
 
-var removedItem = fruits.splice(pos, 1); // this is how to remove an item
+//var removedItem = fruits.splice(pos, 1); // this is how to remove an item
                                         
 // ["Strawberry", "Mango"]
 
 //Remove items from an index position
 
-var vegetables = ['Cabbage', 'Turnip', 'Radish', 'Carrot'];
-console.log(vegetables); 
+//var vegetables = ['Cabbage', 'Turnip', 'Radish', 'Carrot'];
+//console.log(vegetables); 
 // ["Cabbage", "Turnip", "Radish", "Carrot"]
 
-var pos = 1, n = 2;
+//var pos = 1, n = 2;
 
-var removedItems = vegetables.splice(pos, n); 
+//var removedItems = vegetables.splice(pos, n); 
 // this is how to remove items, n defines the number of items to be removed,
 // from that position(pos) onward to the end of array.
 
-console.log(vegetables); 
+//console.log(vegetables); 
 // ["Cabbage", "Carrot"] (the original array is changed)
 
-console.log(removedItems); 
+//console.log(removedItems); 
 // ["Turnip", "Radish"]
 
 //Copy an Array
 
-var shallowCopy = fruits.slice(); // this is how to make a copy
-// ["Strawberry"]
+//var shallowCopy = fruits.slice(); // this is how to make a copy
+// ["Strawberry"] 
